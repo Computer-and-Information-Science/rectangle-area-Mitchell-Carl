64360e309a5e694edadcc9c247c39f5bf0598f6f
Mitchell-Carl <Mitchell.carl@student.northampton.edu>
2026-02-02 00:45:29 +0000

    Wrote Rectangle Properties Code

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
+  double Area, Perimeter;
+  Area = length * width;
+  Perimeter = 2*(length + width); 
 
+  cout << "Rectangle Properties:" << endl;
+  cout << "The Length = " << length << endl;
+  cout << "The Width = " << width << endl;
+  cout << "The Area = " << Area << endl;
+  cout << "The Perimeter = " << Perimeter << endl;
 
 }
